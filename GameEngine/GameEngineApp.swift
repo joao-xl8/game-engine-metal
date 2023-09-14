cd6e099d58c8a60c804d2e74ad2389c52fa845b7
joao_arnold.py <joaopaulo167@gmail.com>
2023-09-14 20:17:19 -0300

    Initial Commit

---
+++ GameEngine/GameEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameEngineApp.swift
+//  GameEngine
+//
+//  Created by João Paulo Arnold Barros on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct GameEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
